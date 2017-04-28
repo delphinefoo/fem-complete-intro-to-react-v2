dcb44b05e8ebf890d453b70985de623e96613e5e
Delphine Foo-Matkin <delphinefoo@users.noreply.github.com>
2017-04-27 19:13:36 -0700

    Code cleanup

--- js/Search.js
+++ js/Search.js
@@ -5,7 +5,7 @@ import preload from '../public/data.json'
 const Search = React.createClass({
   getInitialState () {
     return {
-      searchTerm: 'this is the default string'
+      searchTerm: ''
     }
   },
   handleSearchTermChange (event) {
@@ -20,11 +20,9 @@ const Search = React.createClass({
         </header>
         <div>
           {preload.shows
-            .filter((show) => {
-              return `${show.title} ${show.description}`
+            .filter((show) => `${show.title} ${show.description}`
                 .toUpperCase().indexOf(this.state.searchTerm
-                .toUpperCase()) >= 0
-            })
+                .toUpperCase()) >= 0)
             .map((show) => {
               return (
                 <ShowCard key={show.imdbID} {...show} />
