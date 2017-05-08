c2915dd042771589d1c67018c2ece7d7ad8bc891
Delphine Foo-Matkin <delphinefoo@users.noreply.github.com>
2017-05-07 18:12:32 -0700

    Added link to showcards

--- js/ShowCard.js
+++ js/ShowCard.js
@@ -1,22 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router'
 const { string } = React.PropTypes
 
 const ShowCard = React.createClass({
   propTypes: {
-    poster: string,
-    title: string,
-    year: string,
-    description: string
+    poster: string.isRequired,
+    title: string.isRequired,
+    year: string.isRequired,
+    description: string.isRequired,
+    imdbID: string.isRequired
   },
   render () {
     const { poster, title, year, description } = this.props
     return (
-      <div className='show-card'>
-        <img src={`/public/img/posters/${poster}`} />
-        <h3>{title}</h3>
-        <h4>({year})</h4>
-        <p>{description}</p>
-      </div>
+      <Link to={`/details/${this.props.imdbID}`}>
+        <div className='show-card'>
+          <img src={`/public/img/posters/${poster}`} />
+          <h3>{title}</h3>
+          <h4>({year})</h4>
+          <p>{description}</p>
+        </div>
+      </Link>
     )
   }
 })
