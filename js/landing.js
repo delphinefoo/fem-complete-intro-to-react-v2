7b41aa66e2c464fc14ed9cd32bd1b07232318e53
Delphine Foo-Matkin <delphinefoo@users.noreply.github.com>
2017-05-14 17:49:48 -0700

    Added event listener to search input

--- js/landing.js
+++ js/landing.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router'
-const { string } = React.PropTypes
+import { setSearchTerm } from './actionCreators'
+const { string, func } = React.PropTypes
 
 const Landing = React.createClass({
   propType: {
-    searchTerm: string
+    searchTerm: string,
+    dispatch: func
+  },
+  handleSearchTermChange (event) {
+    // dispatch comes from 'connect'
+    // it dispatches an action to the rootReducer
+    this.props.dispatch(setSearchTerm(event.target.value))
   },
   render () {
     return (
       <div className='landing'>
         <h1>DelFlix</h1>
-        <input value={this.props.searchTerm} type='text' placeholder='Search' />
+        <input onChange={this.handleSearchTermChange} value={this.props.searchTerm} type='text' placeholder='Search' />
         <Link to='/search'>or Browse All</Link>
       </div>
     )
